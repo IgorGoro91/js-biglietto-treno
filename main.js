06c37a25a40cff4fce1c2c576039ef78b7741dfa
IgorGoro91 <igorysc@gmail.com>
2025-01-10 19:08:39 +0100

    add:correzione

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
-// preparazione 
-// eta e chilolometri 
-let parolaEta = prompt("quanti anni hai")
-let parolaKm = prompt("quanti chilometri percorri")
+/*
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+- L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+*/
 
 
+const prezzoBigleto = 0.21
 
-console.log(parolaEta, parolaKm)
+let km = prompt( " Quanti Km percorri") 
+let eta =  prompt("Quanti anni hai") 
 
+let sconto18 = 0.8
+let sconto65 = 0.6
 
-let euro = 0.21
-let costoTot ;
- 
-if( parolaKm > 0) {
-    costoTot = parolaKm * euro;
-    console.log ( `${costoTot} costo dell biglieto`)
-}
+let prezzoTotale;
 
-if(parolaEta < 18){
-    costoScontato = costoTot * 0.8;
-    console.log( `${costoScontato} costo scontato anni 18`)
-}
-
-if(parolaEta > 65){
-    costoScontato = costoTot * 0.6;
-    console.log( `${costoScontato} costo scontato anni 65`)
-}
+console.log( `${km} =I tuoi Km:`,
+     `${eta} =la tua eta:`,
+     `${km * prezzoBigleto} = il prezzo bigletto` )
 
 
+     if( eta < 18) {
+        prezzoTotale = (prezzoBigleto * km) * sconto18
+        
+     } else if(eta > 65){
+        prezzoTotale = (prezzoBigleto * km) * sconto65
+        
+     } else{
+        prezzoTotale = (prezzoBigleto * km)
+     }
 
-
+     console.log(`il tuo sconto ${prezzoTotale.toFixed(2)} euro`)
